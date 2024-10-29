42132ec116809cbc7fbe3c1377ff61af25dbd911
Paula Ashraf <paula.a.pham@gmail.com>
2024-10-29 22:01:46 +0300

    setup project structure

---
+++ src/app/modules/chatto/chatto.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ChattoRoutingModule } from './chatto-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ChattoRoutingModule
+  ]
+})
+export class ChattoModule { }
